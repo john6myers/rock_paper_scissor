7af312f0c442ab485a4a0cc60e815fa7641fbaf4
John Myers <john6myers@gmail.com>
2022-06-01 01:39:31 -0400

    working on debugging and getting the game to play 5 rounds

--- myscript.js
+++ myscript.js
@@ -1,6 +1,48 @@
-function computerPlay() {
-    var word = ['Rock','Paper','Scissors'];
-    var randomChoice = word[Math.floor(Math.random()*word.length)];
-    alert('The computer chose:' + randomChoice);
+// 3 choices that stay constant 
+const choices = ['rock','paper', 'scissors'];
+const playerSelection = userPlay()
+const computerSelection  = computerPlay();
+
+// function that plays five rounds of the game, and returns the results of the winner after the five rounds
+
+function game(playRound) {
+  
 }
-console.log(computerPlay())
+
+
+// Function that grabs the input from userPlay and grabs the input from computerPlay and 
+// returns a result, either win, lose or tie!
+
+function playRound(playerSelection, computerSelection) {
+  
+  if (playerSelection === computerSelection) {
+    return "tie";
+  
+  } else if (
+    (playerSelection === "rock" && computerSelection == "scissors") ||
+    (playerSelection === "paper" && computerSelection == "rock") ||
+    (playerSelection === "scissors" && computerSelection == "paper")
+  )
+    
+    {
+      return "Player wins!";
+    } else {
+      return "The computer WINS REEEEEE";
+    }
+}    
+  
+// userPlay function that prompt the user to type in either rock, paper or scissors!
+
+ function userPlay(){
+   let input = prompt("Pick rock paper or scissors");
+   return input;
+ }
+
+ // computerPlay function that takes an item from the given array and returns a random result
+
+ function computerPlay() {
+   return choices[Math.floor(Math.random()*choices.length)];
+ }
+
+
+console.log(playRound(playerSelection, computerSelection));
